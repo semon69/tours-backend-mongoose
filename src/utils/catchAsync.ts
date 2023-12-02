cbb3d1de37d47c046fab9d515b1a5118bd129d8a
unknown <smdemon3@gmail.com>
2023-12-02 09:28:30 -0800

    solve problem in catchAsync function

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,19 +1,19 @@
-// import { NextFunction, Request, RequestHandler, Response } from 'express';
-
-// const catchAsync = async (func: RequestHandler) => {
-//   return (req: Request, res: Response, next: NextFunction) => {
-//     Promise.resolve(func(req, res, next)).catch((err) => next(err))
-//   };
-// };
-
-// export default catchAsync;
-
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-const catchAsync = (fn: RequestHandler) => {
+const catchAsync = (func: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    Promise.resolve(func(req, res, next)).catch((err) => next(err))
   };
 };
 
 export default catchAsync;
+
+// import { NextFunction, Request, RequestHandler, Response } from 'express';
+
+// const catchAsync = (fn: RequestHandler) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// export default catchAsync;
